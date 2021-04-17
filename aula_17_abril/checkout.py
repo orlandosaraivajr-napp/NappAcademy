40387c27b777d85cdfb03ca50661b339eaa5a7c4
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-04-17 12:04:30 -0300

    Método checkout v.2

--- aula_17_abril/checkout.py
+++ aula_17_abril/checkout.py
@@ -1,25 +1,70 @@
-class CarrinhoCompra:
-    def checkout(self, valor_compra, cartao_fidelidade, profissao, cupom):
-        if cartao_fidelidade == 'ouro' and profissao == 'professor':
+class CheckoutStrategy:
+    def calculo(self, valor_compra, dados):
+        if dados.cartao == 'ouro' and dados.profissao == 'professor':
             return valor_compra * 0.5
-        if profissao == 'professor':
+        if dados.profissao == 'professor':
             return valor_compra * 0.9
-        if profissao == 'estudante':
+        if dados.profissao == 'estudante':
             return valor_compra * 0.5
-        if cartao_fidelidade == 'ouro':
+        if dados.cartao == 'ouro':
             return valor_compra * 0.75
-        if cupom == 'napp2021':
+        if dados.cupom == 'napp2021':
             return valor_compra * 0.7
-        if cupom == 'napp21':
+        if dados.cupom == 'napp21':
             return valor_compra * 0.8
         return valor_compra
 
+class CarrinhoCompra:
+    def checkout(self, valor_compra, dados):
+        checkout = CheckoutStrategy()
+        return checkout.calculo(valor_compra, dados)
+
+class DadosCliente:
+    def __init__(self):
+        self._cartao_fidelidade = None
+        self._profissao = None
+        self._cupom = None
+    
+    @property
+    def cartao(self):
+        return self._cartao_fidelidade
+
+    @cartao.setter
+    def cartao(self, value):
+        self._cartao_fidelidade = value
+    
+    @property
+    def profissao(self):
+        return self._profissao
+
+    @profissao.setter
+    def profissao(self, value):
+        self._profissao = value
+
+    @property
+    def cupom(self):
+        return self._cupom
+
+    @cupom.setter
+    def cupom(self, value):
+        self._cupom = value
+
 
 carrinho = CarrinhoCompra()
-assert(carrinho.checkout(1000, None, None, None) == 1000)
-assert(carrinho.checkout(1000, None, 'professor', None) == 900)
-assert(carrinho.checkout(1000, None, 'estudante', None) == 500)
-assert(carrinho.checkout(1000, None, None, 'napp2021') == 700)
-assert(carrinho.checkout(1000, None, None, 'napp21') == 800)
-assert(carrinho.checkout(1000, 'ouro', None, None) == 750)
-assert(carrinho.checkout(1000, 'ouro', 'professor', None) == 500)
+dados = DadosCliente()
+assert(carrinho.checkout(1000, dados) == 1000)
+dados.profissao = 'professor'
+assert(carrinho.checkout(1000, dados) == 900)
+dados.profissao = 'estudante'
+assert(carrinho.checkout(1000, dados) == 500)
+dados.profissao = None
+dados.cupom = 'napp2021'
+assert(carrinho.checkout(1000, dados) == 700)
+dados.cupom = 'napp21'
+assert(carrinho.checkout(1000, dados) == 800)
+dados.cupom = None
+dados.cartao = 'ouro'
+assert(carrinho.checkout(1000, dados) == 750)
+dados.cartao = 'ouro'
+dados.profissao = 'professor'
+assert(carrinho.checkout(1000, dados) == 500)
