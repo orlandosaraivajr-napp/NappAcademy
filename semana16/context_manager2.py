9ab1581e93c30849c1855f17d1f1493051b3269e
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-05-17 10:05:13 -0300

    Ajuste semana 16

--- semana16/context_manager2.py
+++ semana16/context_manager2.py
@@ -1,11 +1,11 @@
 import contextlib
 
 @contextlib.contextmanager
-def file_hanlder(file_name,file_mode):
+def file_handler(file_name,file_mode):
     file = open(file_name,file_mode)
     yield file
     file.close()
 
 if __name__ == "__main__":
-   with file_hanlder("cm2.txt","w") as f:
+   with file_handler("cm2.txt","w") as f:
        f.write("Test")
