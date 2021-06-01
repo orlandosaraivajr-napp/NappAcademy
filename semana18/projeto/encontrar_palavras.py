8f592f4a830746716adb3b9c327c38e2b3fda65d
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2021-06-01 10:29:04 -0300

    Atividade Semana 18

---
+++ semana18/projeto/encontrar_palavras.py
@@ -0,0 +1,24 @@
+import os
+import glob
+
+def palavra_no_arquivo(palavra, arquivo):
+    with open(arquivo, 'r') as f:
+        for line in f:
+            return palavra in line
+    return False
+
+def todos_arquivos_txt():
+    looking_for = '**/*.txt'
+    matched = glob.glob(looking_for, recursive=True)
+    return matched
+
+def encontrar_palavra(palavra):
+    encontrado_em = []
+    arquivos = todos_arquivos_txt()
+    for arquivo in arquivos:
+        if palavra_no_arquivo(palavra, arquivo):
+            encontrado_em.append(arquivo)
+    return encontrado_em
+
+busca_napp1 = encontrar_palavra('napp')
+busca_napp2 = encontrar_palavra('NaPp')
